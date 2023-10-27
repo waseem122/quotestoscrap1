f9a2539decd9989ce221a25562ba25da796330c2
Muhammad Waseem <mw11221122@gmail.com>
2023-10-26 17:01:33 -0700

    scrap title

---
+++ quote.py
@@ -0,0 +1,8 @@
+#This is quotes to scrap website scraping file
+#x
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://quotes.toscrape.com/')
+soup = BeautifulSoup(res.content, 'html.parser')
+print(soup.title.text)
