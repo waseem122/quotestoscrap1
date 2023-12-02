9cdb1b5c04423f620755587f2d69dc0b9b637f21
Muhammad Waseem <mw11221122@gmail.com>
2023-12-02 03:08:55 -0800

    This scraper scrap all quotes and authors from this website with pagination

--- quote.py
+++ quote.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-url = "https://quotes.toscrape.com/"
-res = requests.get(url)
-if res.status_code == 200:
-    #parse the webpage
+base_url = 'https://quotes.toscrape.com/page/{}/'
+
+for i in range(1, 11):
+    url = base_url.format(i)
+    res = requests.get(url)
     soup = BeautifulSoup(res.content, 'html.parser')
-    print(soup.title)
+    quotes = soup.find_all('span', class_ = 'text')
+    author = soup.find_all('small', class_ = 'author')
+   
+    for x, y in zip(quotes, author):
+        print(x.text)
+        print(y.text)
 
-else:
-    print('webpage is not there')
+#x
